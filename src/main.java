e01c57744f0297d70afb588ca5a197dc2b80eb2a
NAleshina <minerva987@gmail.com>
2020-07-10 21:49:14 +0300

    Created Miles

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        int ticketPrice = 140;
+        int miles = ticketPrice / 20;
+        System.out.println(miles);
+    }
+}
+
